334213bfa5b155ff2f0d63c4f082b0bf141f26d1
Sankar S <sankar_subramanian@infosys.com>
2016-11-23 15:34:57 +0530

    Spaces used for indentation

--- examples/post_device_update.py
+++ examples/post_device_update.py
@@ -5,7 +5,7 @@ from m2x_mqtt.client import M2XClient
 This example demonstrates how to update single values to multiple streams to target device
 
 API Documentation:
-https://m2x.att.com/developer/documentation/v2/device#Post-Device-Update--Single-Values-to-Multiple-Streams-
+"https://m2x.att.com/developer/documentation/v2/device#Post-Device-Update--Single-Values-to-Multiple-Streams-"
 '''
 
 API_KEY = os.environ['API_KEY']
@@ -15,11 +15,11 @@ client = M2XClient(key=API_KEY)
 device = client.device(DEVICE)
 
 params = {
-	"timestamp" : "<YOUR-TIME-IN-ISO-8601-FORMAT>", 
-	"values" : {
-		"<YOUR-EXISTING-STREAM-NAME>": "<YOUR-NEW-STREAM-VALUE>",
-   		"<YOUR-EXISTING-STREAM-NAME>" : "<YOUR-NEW-STREAM-VALUE>"
-   	}
+    "timestamp" : "<YOUR-TIME-IN-ISO-8601-FORMAT>",
+    "values" : {
+        "<YOUR-EXISTING-STREAM-NAME>": "<YOUR-NEW-STREAM-VALUE>",
+        "<YOUR-EXISTING-STREAM-NAME>" : "<YOUR-NEW-STREAM-VALUE>"
+    }
 }
 
 response = device.post_device_update(data = params)
